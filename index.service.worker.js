6f7d0ef05e2a97701931216c74cbe64353674b47
Solid Rock deployment <actions@users.noreply.github.com>
2026-08-11 23:37:50 +0000

    Deploy 033aae46df8ca3fe61f2c0218b97eac66d614cb5

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786483910|4085561';
+const CACHE_VERSION = '1786491459|4011644';
 /** @type {string} */
 const CACHE_PREFIX = 'Solid Rock: Refo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
